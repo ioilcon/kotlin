9e45e4ebda9e21f01d28d29c987f09c9ffa9922e
ioilcon <Zdvadim98@gmail.com>
2021-05-22 00:34:09 +0400

    Задания 6-8

--- Lab1_Introduction/src/lab1.kt
+++ Lab1_Introduction/src/lab1.kt
@@ -1,4 +1,7 @@
 import kotlin.math.sqrt
+import kotlin.math.max
+import kotlin.math.min
+
 
 //1, 2
 fun helloWorld() = println("Hello Word")
@@ -15,7 +18,39 @@ fun helloUser(name : String) {
     }
 }
 
+//6
+fun digitsSum(number : Int) = digitsProcessing(number, 0, {a, b -> a + b}, {_ -> true})
 
+//7
+fun digitsMax(number : Int) = digitsProcessing(number, 0, {a, b -> if (a > b) a else b}, {_ -> true})
+
+fun digitsMin(number : Int) = digitsProcessing(number, 9, {a, b -> if (a < b) a else b}, {_ -> true})
+
+fun digitsMul(number : Int) = digitsProcessing(number, 1, {a, b -> a * b}, {_ -> true})
+
+//8
+fun isSimple(number : Int) : Boolean = isSimple(number, 2, sqrt(number.toDouble()))
+tailrec fun isSimple(number : Int, current : Int, border : Double) : Boolean = when {
+        current > border -> true
+        number % current == 0 -> false
+        else -> isSimple(number, current + 1, border)
+    }
+
+fun GCD(first : Int, second : Int) : Int = when {
+    first == second -> first
+    first == 0 || second == 0 -> 0
+    else -> GCD(max(first, second) - min(first, second), min(first, second))
+}
+
+fun digitsNotSimpleSum(number : Int) = digitsProcessing(number, 0, {a, b -> a + b}, {a -> !isSimple(a)})
+
+fun digitsCount3(number : Int) = digitsProcessing(number, 0, {a, b -> a + 1}, {a -> a < 3})
+
+fun smtnStrange(number : Int) : Int = smtnStrange(number, 2, digitsProcessing(number, 0, {a, b -> a + b}, {a -> isSimple(a)}), 0)
+tailrec fun smtnStrange(numberInt : Int, current : Int, simpleDigitsSum : Int, accumulator : Int) = 5
+tailrec fun digitsProcessing(number : Int, accumulator : Int, func : (Int, Int) -> Int, pr : (Int) -> Boolean) : Int =
+    if (number == 0) accumulator else
+        digitsProcessing(number / 10, if (pr(number % 10)) func(number % 10, accumulator) else accumulator, func, pr)
 
 fun main() {
     helloWorld()
