643738f03536fad2334e4e658b6190e8972fa116
ioilcon <Zdvadim98@gmail.com>
2021-05-23 22:29:42 +0400

    Исправил рекурсию вверх/вниз

--- Lab2_Recursion/src/lab2.kt
+++ Lab2_Recursion/src/lab2.kt
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 //1
-fun digitsSumDown(number : Int) = digitsProcessingDown(number, 0, {a, b -> a + b}, {_ -> true})
+fun digitsSumUp(number : Int) = digitsProcessingUp(number, 0, {a, b -> a + b}, {_ -> true})
 
-fun digitsProcessingDown(number : Int, accumulator : Int, func : (Int, Int) -> Int, pr : (Int) -> Boolean) : Int =
+fun digitsProcessingUp(number : Int, accumulator : Int, func : (Int, Int) -> Int, pr : (Int) -> Boolean) : Int =
     when {
         number == 0 -> accumulator
-        pr(number % 10) -> func(number % 10, digitsProcessingDown(number / 10, accumulator, func, pr))
-        else -> digitsProcessingDown(number / 10, accumulator, func, pr)
+        pr(number % 10) -> func(number % 10, digitsProcessingUp(number / 10, accumulator, func, pr))
+        else -> digitsProcessingUp(number / 10, accumulator, func, pr)
     }
 
 fun main() {
     print("Insert number: ")
     val input = Scanner(System.`in`)
     val number : Int = input.nextInt()
-    println(digitsSumDown(number))
+    println(digitsSumUp(number))
 }
