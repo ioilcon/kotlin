8d099bdbe19e9326ca60560d8ba29f9b940bc44f
ioilcon <Zdvadim98@gmail.com>
2021-05-24 01:30:55 +0400

    Задание 2

--- Lab2_Recursion/src/lab2.kt
+++ Lab2_Recursion/src/lab2.kt
@@ -3,12 +3,21 @@ import java.util.Scanner;
 //1
 fun digitsSumUp(number : Int) = digitsProcessingUp(number, 0, {a, b -> a + b}, {_ -> true})
 
+//2
+fun digitsDumDown(number : Int) = digitsProcessingDown(number, 0, {a, b -> a + b}, {_ -> true})
+
+//3
+
 fun digitsProcessingUp(number : Int, accumulator : Int, func : (Int, Int) -> Int, pr : (Int) -> Boolean) : Int =
     when {
         number == 0 -> accumulator
         pr(number % 10) -> func(number % 10, digitsProcessingUp(number / 10, accumulator, func, pr))
         else -> digitsProcessingUp(number / 10, accumulator, func, pr)
     }
+
+tailrec fun digitsProcessingDown(number : Int, accumulator : Int, func : (Int, Int) -> Int, pr : (Int) -> Boolean) : Int =
+    if (number == 0) accumulator else
+        digitsProcessingDown(number / 10, if (pr(number % 10)) func(number % 10, accumulator) else accumulator, func, pr)
 
 fun main() {
     print("Insert number: ")
