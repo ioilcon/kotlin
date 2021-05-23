a676337f0fa5d2b940e750014c81ea27564ba71c
ioilcon <Zdvadim98@gmail.com>
2021-05-24 01:41:34 +0400

    Задание 4

--- Lab2_Recursion/src/lab2.kt
+++ Lab2_Recursion/src/lab2.kt
@@ -1,34 +1,36 @@
 import java.util.Scanner;
 
 //1
-fun digitsSumUp(number : Int) = digitsProcessingUp(number, 0, {a, b -> a + b}, {_ -> true})
+fun digitsSumUp(number : Int) = digitsProcessingUp(number, 0, {a, b -> a + b})
 
 //2
-fun digitsDumDown(number : Int) = digitsProcessingDown(number, 0, {a, b -> a + b}, {_ -> true})
+fun digitsDumDown(number : Int) = digitsProcessingDown(number, 0, {a, b -> a + b})
 
 //3
-fun digitsMulUp(number : Int) = digitsProcessingUp(number, 1, {a, b -> a * b}, {_ -> true})
+fun digitsMulUp(number : Int) = digitsProcessingUp(number, 1, {a, b -> a * b})
 
-fun digitsMinUp(number : Int) = digitsProcessingUp(number, 9, {a, b -> if(a < b) a else b}, {_ -> true})
+fun digitsMinUp(number : Int) = digitsProcessingUp(number, 9, {a, b -> if(a < b) a else b})
 
-fun digitsMaxUp(number : Int) = digitsProcessingUp(number, 0, {a, b -> if(a > b) a else b}, {_ -> true})
+fun digitsMaxUp(number : Int) = digitsProcessingUp(number, 0, {a, b -> if(a > b) a else b})
 
-fun digitsMulDown(number : Int) = digitsProcessingDown(number, 1, {a, b -> a * b}, {_ -> true})
+fun digitsMulDown(number : Int) = digitsProcessingDown(number, 1, {a, b -> a * b})
 
-fun digitsMinDown(number : Int) = digitsProcessingDown(number, 9, {a, b -> if(a < b) a else b}, {_ -> true})
+fun digitsMinDown(number : Int) = digitsProcessingDown(number, 9, {a, b -> if(a < b) a else b})
 
-fun digitsMaxDown(number : Int) = digitsProcessingDown(number, 0, {a, b -> if(a > b) a else b}, {_ -> true})
+fun digitsMaxDown(number : Int) = digitsProcessingDown(number, 0, {a, b -> if(a > b) a else b})
 
-//4
+//4 - digitsProcessingUp и digitsProcessingDown выполнябт заданный функционал
 
-fun digitsProcessingUp(number : Int, accumulator : Int, func : (Int, Int) -> Int, pr : (Int) -> Boolean) : Int =
+//5
+
+fun digitsProcessingUp(number : Int, accumulator : Int, func : (Int, Int) -> Int, pr : (Int) -> Boolean = {_ -> true}) : Int =
     when {
         number == 0 -> accumulator
         pr(number % 10) -> func(number % 10, digitsProcessingUp(number / 10, accumulator, func, pr))
         else -> digitsProcessingUp(number / 10, accumulator, func, pr)
     }
 
-tailrec fun digitsProcessingDown(number : Int, accumulator : Int, func : (Int, Int) -> Int, pr : (Int) -> Boolean) : Int =
+tailrec fun digitsProcessingDown(number : Int, accumulator : Int, func : (Int, Int) -> Int, pr : (Int) -> Boolean = {_ -> true}) : Int =
     if (number == 0) accumulator else
         digitsProcessingDown(number / 10, if (pr(number % 10)) func(number % 10, accumulator) else accumulator, func, pr)
 
